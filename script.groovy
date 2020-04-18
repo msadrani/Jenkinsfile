743fb521b02de1510ba8356dbc0c4385234e2012
msadrani <meetsadrani56@gmail.com>
2020-04-18 18:08:24 +0530

    adding groovy

---
+++ script.groovy
@@ -0,0 +1,9 @@
+def buildapp(){
+	            echo "builidng my application"
+                echo "version of this file are ${VERSION}"
+				echo "parameters ${params.VERSION1}"
+}
+def deployapp(){
+				echo "Deploying my application"
+}
+return this
